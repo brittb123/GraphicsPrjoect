e5f5e55f43c50876fc213a1b318f67f20476f656
Britt <brittboudreaux@yahoo.com>
2021-09-30 16:59:06 -0500

    Added start update draw and end to entity class

--- GraphicsProject/Entity.h
+++ GraphicsProject/Entity.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "glm/mat4x4.hpp"
+
 class Entity
 {
+public:
+	Entity() {}
+	~Entity() {}
+
+	void start();
+	void update();
+	void draw();
+	void end();
+
+private:
+	glm::mat4 m_transform;
+
 };
 
